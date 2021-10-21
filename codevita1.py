74511042b10ec54ddc3a0f39513f65835c276e43
komalkolani <72454798+komalkolani@users.noreply.github.com>
2021-10-21 17:12:29 +0530

    Add files via upload

---
+++ codevita1.py
@@ -0,0 +1,25 @@
+def isPrime(n):
+    b = True
+    for x in range(2,n-1):
+        if(n%x==0):
+            b = False
+            break
+    return b
+
+totalHr , division = map(int,input().split())
+peroid = int(totalHr /division)
+counter = 0
+for i in range(2,peroid):
+    if isPrime(i):
+        for j in range(1,division):
+            if isPrime(peroid*j+i):
+                if totalHr - (peroid*j+i) < peroid :
+                    counter+=1
+                    break
+                continue
+            else:
+                break
+
+print(counter,end="")
+
+
